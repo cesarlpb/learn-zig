2742ab30a221f37040f9cbbfeb2e546d67502c1c
César <cesarlpb@gmail.com>
2024-03-03 14:12:52 +0100

    script actualizado: temas y ejercicios

--- src/index.js
+++ src/index.js
@@ -2,54 +2,54 @@ import { readFileSync, createWriteStream } from 'fs';
 import { createCanvas, loadImage } from 'canvas';
 import path from 'path';
 
-// Función para crear el grid de rectángulos y colorear los temas completos
-function crearGrid(numTemas, lineas, progreso) {
-    // Tamaño del canvas
+function crearGrid(numCompletados, total, tipo) {
     const width = 400;
     const height = 400;
-
-    // Crear el canvas
     const canvas = createCanvas(width, height);
     const ctx = canvas.getContext('2d');
 
-    // Calcular el número de rectángulos en el grid
-    const totalRectangulos = lineas.length / 2;
+    const totalRectangulos = total.length / 2;
+    const completados = Math.floor((numCompletados / totalRectangulos) * totalRectangulos);
 
-    // Calcular el número de rectángulos completos
-    const temasCompletos = Math.floor((numTemas / totalRectangulos) * totalRectangulos);
-
-    // Ancho y alto de los rectángulos
     const rectWidth = width / 10;
     const rectHeight = height / 10;
 
-    // Colorear los rectángulos completos
     ctx.fillStyle = 'green';
-    for (let i = 0; i < temasCompletos; i++) {
+    for (let i = 0; i < completados; i++) {
         const row = Math.floor(i / 10);
         const col = i % 10;
         ctx.fillRect(col * rectWidth, row * rectHeight, rectWidth, rectHeight);
     }
 
-    // Guardar la imagen en un archivo
-    const out = createWriteStream(path.join(process.cwd(), 'grid.png'));
+    const filename = tipo === "ejercicios" ? 'grid2.png' : 'grid.png';
+    const out = createWriteStream(path.join(process.cwd(), filename));
     const stream = canvas.createPNGStream();
     stream.pipe(out);
     out.on('finish', () => console.log('La imagen se ha guardado correctamente.'));
 }
 
-function actualizarReadme(temasCompletos, temas, progreso) {
+function actualizarReadme(numCompletados, total, progreso, tipo) {
     const readmePath = path.join(process.cwd(), 'README.md');
     const readme = readFileSync(readmePath, 'utf8');
     const lineas = readme.split('\n');
-    const progresoLinea = lineas.findIndex(linea => linea.trim().startsWith('## Progreso'));
+    let progresoLinea = -1;
 
-    if (progresoLinea !== -1) {
-        lineas[progresoLinea] = `## Progreso: ${progreso.toFixed(2)}%`;
+    if (tipo === "ejercicios") {
+        progresoLinea = lineas.findIndex(linea => linea.trim().startsWith('## Progreso ejercicios'));
+    } else {
+        progresoLinea = lineas.findIndex(linea => linea.trim().startsWith('## Progreso contenidos'));
     }
 
-    const temasLinea = lineas.findIndex(linea => linea.trim().startsWith('De un total de'));
+    if (progresoLinea !== -1) {
+        lineas[progresoLinea] = `## Progreso${tipo === "ejercicios" ? " ejercicios" : " contenidos"}: ${progreso.toFixed(2)}%`;
+    }
+
+    const totalText = tipo === "ejercicios" ? "ejercicios" : "temas";
+    const completadosText = tipo === "ejercicios" ? "ejerciciosCompletos" : "temasCompletos";
+
+    const temasLinea = lineas.findIndex(linea => linea.trim().startsWith(`De un total de xx ${totalText}`));
     if (temasLinea !== -1) {
-        lineas[temasLinea] = `De un total de ${temas.length} temas, se han completado ${temasCompletos.length}. Esto representa un progreso del ${progreso.toFixed(2)}%.`;
+        lineas[temasLinea] = `De un total de ${total.length} ${totalText}, se han completado ${numCompletados.length}. Esto representa un progreso del ${progreso.toFixed(2)}% en los ${totalText}.`;
     }
 
     const nuevoReadme = lineas.join('\n');
@@ -58,18 +58,22 @@ function actualizarReadme(temasCompletos, temas, progreso) {
 }
 
 try {
-    const data = readFileSync('src/contenidos.txt', 'utf8');
-    const temas = data.split('\n');
+    const temas = readFileSync('src/contenidos.txt', 'utf8').split('\n');
     const temasCompletos = temas.filter(tema => tema.trim().startsWith('X'));
-    const progreso = (temasCompletos.length / temas.length) * 100;
+    const progresoTemas = (temasCompletos.length / temas.length) * 100;
 
-    // Ejemplo de uso
-    console.log('Temas completos:', temasCompletos);
-    console.log('Temas:', temas);
-    console.log('Progreso:', progreso.toFixed(2) + '%');
+    const ejercicios = readFileSync('src/ejercicios.txt', 'utf8').split('\n');
+    const ejerciciosCompletos = ejercicios.filter(ex => ex.trim().startsWith('X'));
+    const progresoEjercicios = (ejerciciosCompletos.length / ejercicios.length) * 100;
 
-    crearGrid(temasCompletos.length, temas, progreso);
-    actualizarReadme(temasCompletos, temas, progreso);
+    console.log('Progreso temas:', progresoTemas.toFixed(2) + '%');
+    console.log('Progreso ejercicios:', progresoEjercicios.toFixed(2) + '%');
+
+    crearGrid(temasCompletos.length, temas, progresoTemas, "");
+    actualizarReadme(temasCompletos, temas, progresoTemas, "");
+
+    crearGrid(ejerciciosCompletos.length, ejercicios, progresoEjercicios, "ejercicios");
+    actualizarReadme(ejerciciosCompletos, ejercicios, progresoEjercicios, "ejercicios");
 } catch (error) {
     console.error('Error al leer el archivo:', error);
 }
